a3502f4d7d4e5dd540b78a9b5055719ac1b1a182
Piotr Bystron <piotr.bystron@student.wsb.edu.pl>
2026-07-05 15:56:59 +0200

    Update Playwright tests for Login Page

--- pages/login_page.py
+++ pages/login_page.py
@@ -2,13 +2,39 @@ from playwright.sync_api import Page
 
 
 class LoginPage:
-
     URL = "https://the-internet.herokuapp.com/login"
 
     def __init__(self, page: Page):
         self.page = page
         self.h2_text = page.locator("h2")
+        self.flash_message = page.locator("#flash")
 
+        self.username_input = page.get_by_label("Username")
+        self.password_input = page.get_by_label("Password")
+        
+        self.login_button = page.locator(".radius")
+        self.logout_button = page.locator(".button.secondary.radius")
+
+        self.correct_username = "tomsmith"
+        self.correct_password = "SuperSecretPassword!"
+
+        self.invalid_username = "test"
+        self.invalid_password = "123!"
 
     def open(self):
         self.page.goto(self.URL)
+
+    def get_flash_text(self):
+        return self.flash_message.text_content()
+    
+    def flash_should_contain(self, text: str):
+        from playwright.sync_api import expect
+        expect(self.flash_message).to_contain_text(text)
+
+    def submit_login(self, username: str, password: str):
+        self.username_input.fill(username)
+        self.password_input.fill(password)
+        self.login_button.click()
+
+    def submit_logout(self):
+        self.logout_button.click()
--- tests/test_login.py
+++ tests/test_login.py
@@ -7,10 +7,31 @@ def test_login_correct_username_and_password(page):
 
     assert login.h2_text.text_content() == "Login Page"
 
+    login.submit_login(login.correct_username, login.correct_password)
+    login.flash_should_contain("You logged into a secure area!")
 
+def test_login_wrong_username(page):
+    login = LoginPage(page)
+    login.open()
 
-def test_login_wrong_username_and_password(page):
-    pass
+    login.submit_login(login.invalid_username, login.correct_password)
+    login.flash_should_contain("Your username is invalid!")
+
+def test_login_wrong_password(page):
+    login = LoginPage(page)
+    login.open()
+
+    login.submit_login(login.correct_username, login.invalid_password)
+    login.flash_should_contain("Your password is invalid! ")
 
 def test_login_and_logout(page):
-    pass
+    login = LoginPage(page)
+    login.open()
+
+    assert login.h2_text.text_content() == "Login Page"
+
+    login.submit_login(login.correct_username, login.correct_password)
+    login.flash_should_contain("You logged into a secure area!")
+
+    login.submit_logout()
+    login.flash_should_contain("You logged out of the secure area!")
